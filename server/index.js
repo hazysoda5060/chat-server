3455f19323352654661cc52d2678042504126b49
alex <alexanderstapp@gmail.com>
2021-04-28 15:41:56 -0600

    finished lab

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const mc = require('./controllers/messages_controller')
 
-const PORT = 3001
 app.use(express.json())
 
+app.use(express.static(__dirname + '/../public/build'))
+
+const msgUrl = '/api/messages'
+app.get(msgUrl, mc.getMsg)
+app.post(msgUrl, mc.createMsg)
+app.delete(`${msgUrl}/:id`, mc.deleteMsg)
+app.put(`${msgUrl}/:id`, mc.editMsg)
+
+const PORT = 3001
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 })
